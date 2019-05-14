6ccc7e85de48c3158e2d0a87cef2379ec828fa95
kelly-lee <kellyleemz285@163.com>
2019-05-14 19:09:27 +0800

    update

--- nlp_demo/acim/acim.py
+++ nlp_demo/acim/acim.py
@@ -26,20 +26,19 @@ def get_textlines(filename):
     return textlines
 
 
-def clean_text(sentence):
+def clean_text(sentence, punctuation):
     sentence = re.sub(punctuation.decode('utf8'), "".decode('utf8'),
                       sentence.decode('utf8'))
     return sentence
 
 
-def cut_words(sentence):
+def cut_words(sentence, stopwords):
     words = list(jieba.cut(sentence))
-    words = del_stopwords(words)
-    # print ", ".join(words)
+    words = del_stopwords(words, stopwords)
     return words
 
 
-def del_stopwords(words):
+def del_stopwords(words, stopwords):
     return [word for word in words if word not in stopwords]
 
 
@@ -54,6 +53,23 @@ class W2V():
         self.lsi = models.LsiModel(self.tfidf[self.corpus], id2word=self.dictionary, num_topics=300)
         self.similarity = similarities.MatrixSimilarity(self.lsi[self.corpus])
         self.word2vec = Word2Vec(words, min_count=1, window=5, size=200, iter=50)
+        words = [word for doc in self.words for word in doc]
+        global_corpus = self.get_corpus(words)
+        self.init_global_bows(global_corpus)
+        self.init_global_tfidfs(global_corpus)
+
+    def init_global_bows(self, global_corpus):
+        self.global_bows = {}
+        for id, bow in global_corpus:
+            token = self.dictionary.id2token[id]
+            self.global_bows[token] = bow
+
+    def init_global_tfidfs(self, global_corpus):
+        self.global_tfidfs = {}
+        for id, tf in global_corpus:
+            token = self.dictionary.id2token[id]
+            idf = np.log(1.0 * self.tfidf.num_docs / self.tfidf.dfs[id])
+            self.global_tfidfs[token] = tf * idf
 
     def get_vector(self, word):
         return self.word2vec.wv[word]
@@ -67,28 +83,19 @@ class W2V():
     def get_corpus(self, words=None):
         if words is not None:
             words = np.array(words)
-            # # if words.dmin == 1:
-            # words = words.reshape(-1, 1)
+            if words.ndim == 1:
+                return self.dictionary.doc2bow(words)
         else:
             words = self.words
-        return [self.dictionary.doc2bow(word) for word in words]
+        # [[(id1,num1),(id2,num2)],[(id3,num3)]]
+        return [self.dictionary.doc2bow(doc) for doc in words]
 
-    def get_bow(self, keyword=None, topn=None):
+    def get_bows(self):
         bows = [
-            [(self.id2token[word[0]], word[1]) for word in sorted(doc, key=lambda word: -word[1])]
+            [(self.dictionary.id2token[id], num) for id, num in sorted(doc, key=lambda (id, num): -num)]
             for doc in self.corpus
         ]
-        if keyword is not None:
-            bows = bows[keyword]
-        if topn is not None:
-            bows = bows[:topn]
         return bows
-
-    def get_global_bow(self):
-        words = [[word for doc in self.words for word in doc]]
-        global_corpus = self.get_corpus(words)
-        return [(self.dictionary.id2token[word[0]], word[1]) for word in
-                sorted(global_corpus[0], key=lambda word: -word[1])]
 
     def get_tfidf(self, words=None):
         # 在多少文档中出现
@@ -98,17 +105,21 @@ class W2V():
             for doc in self.tfidf[corpus]
         ]
 
-    def get_global_tfidf(self):
-        idfs = self.tfidf.idfs
-        words = [[word for doc in self.words for word in doc]]
-        global_tf = self.get_corpus(words)[0]
-        global_tfidf = {}
-        for key, tf in global_tf:
-            idf = np.log(1.0 * self.tfidf.num_docs / self.tfidf.dfs[key])
-            token = self.dictionary.id2token[key]
-            global_tfidf[token] = tf * idf
-        global_tfidf = sorted(global_tfidf.items(), key=lambda word: word[1], reverse=True)
-        return global_tfidf
+    def get_global_bow(self, keyword=None, topn=None):
+        if keyword is not None:
+            return self.global_bows[keyword]
+        bows = sorted(self.global_bows.items(), key=lambda (token, bow): bow, reverse=True)
+        if topn is not None:
+            bows = bows[:topn]
+        return bows
+
+    def get_global_tfidf(self, keyword=None, topn=None):
+        if keyword is not None:
+            return self.global_tfidfs[keyword]
+        tfidfs = sorted(self.global_tfidfs.items(), key=lambda (token, tfidf): tfidf, reverse=True)
+        if topn is not None:
+            tfidfs = tfidfs[:topn]
+        return tfidfs
 
     def get_similarities(self, words):
         corpus = self.get_corpus(words)
@@ -117,63 +128,105 @@ class W2V():
         sims = sorted(enumerate(sims), key=lambda item: -item[1])
         return sims
 
+    def draw_vector(self, topn):
+        gloabl_tfidf = self.get_global_tfidf(topn=topn)
+        vectors = {}
+        for token, tfidf in gloabl_tfidf:
+            vectors[token] = self.get_vector(token)
+            # print tfidf[0], tfidf[1]
 
-# acim_raw = open('acim.txt').read()
-# print acim_raw
-punctuation = "[\t\n\s]" \
-              "|[,\.\!\?\/_]" \
-              "|[､、，：。！？；．.‧…]" \
-              "|[-~`＠＃＄％＾＆＊＋－＝/\\\]" \
-              "|[~@#￥%&*+——／＼〾〿]+" \
-              "|[\"\'〝〞＂‟“”‘’＇｀‛〃〟„]" \
-              "|[﹏—–〰〜＿]" \
-              "|[\(\)\{\}\[\]\<\>＜＞｛｝［］《》〈〉（）「」『』【】〔〕〖〗〘〙〚〛｟｠｢｣]" \
-              "|[⑴⑵⑶⑷⑸⑹⑺⑻]" \
-              "|[a-zA-Z0-9]"
-jieba.load_userdict("/Users/a1800101471/PycharmProjects/python_demo2/nlp_demo/acim/userdict_acim.txt")
-raw_textlines = get_textlines('/Users/a1800101471/PycharmProjects/python_demo2/nlp_demo/acim/acim.txt')
-stopwords = get_textlines('/Users/a1800101471/PycharmProjects/python_demo2/nlp_demo/acim/stop_word_acim.txt')
+        pca = PCA(n_components=2)
+        vectors_2d = pca.fit_transform(vectors.values())
+        data = []
+        for index, key in enumerate(vectors.keys()):
+            x, y = vectors_2d[index][0].astype(float), vectors_2d[index][1].astype(float)
+            data[index] = [key, x, y]
+        # data = np.array(data)
+        font = FontProperties(fname='/System/Library/Fonts/PingFang.ttc', size=8)
+        for new_vector in data:
+            key, x, y = new_vector[0], new_vector[1], new_vector[2]
+            plt.text(x, y, key, fontproperties=font)
 
-sentences = [clean_text(sentence) for sentence in raw_textlines if (len(sentence) > 0) & (not sentence.isspace())]
-word_matrix = [cut_words(sentence) for sentence in sentences]
+        # print data.min(axis=0)
+        x_min = np.min(np.array(data)[:, 1].astype(float), axis=0) - 0.5
+        x_max = np.max(np.array(data)[:, 1].astype(float), axis=0) + 0.5
+        y_min = np.min(np.array(data)[:, 2].astype(float), axis=0) - 0.5
+        y_max = np.max(np.array(data)[:, 2].astype(float), axis=0) + 0.5
+        plt.ylim(y_min, y_max)
+        plt.xlim(x_min, x_max)
+        plt.show()
 
-# model = word2vec.Word2Vec(word_matrix, min_count=5, size=500)
-# for w in model.wv.most_similar(u"没有"):
-#     print w[0], w[1]
 
-w2v = W2V(word_matrix)
+def get_words():
+    jieba.load_userdict("/Users/a1800101471/PycharmProjects/python_demo2/nlp_demo/acim/userdict_acim.txt")
+    raw_textlines = get_textlines('/Users/a1800101471/PycharmProjects/python_demo2/nlp_demo/acim/acim_s.txt')
+    stopwords = get_textlines('/Users/a1800101471/PycharmProjects/python_demo2/nlp_demo/acim/stop_word_acim.txt')
 
-bows = w2v.get_global_bow()
-for bow in bows[:100]:
-    print bow[0], bow[1]
-print '---------------------------------'
-tfidf = w2v.get_tfidf()
-# for tfidf in tfidf[:50]:
-#     for item in tfidf:
-#         print item[0], item[1]
-print '---------------------------------'
-gloabl_tfidf = w2v.get_global_tfidf()
-vectors = {}
-for tfidf in gloabl_tfidf[:100]:
-    vectors[tfidf[0]] = w2v.get_vector(tfidf[0])
-    print tfidf[0], tfidf[1]
+    punctuation = "[\t\n\s]" \
+                  "|[,\.\!\?\/_]" \
+                  "|[､、，：。！？；．.‧…]" \
+                  "|[-~`＠＃＄％＾＆＊＋－＝/\\\]" \
+                  "|[~@#￥%&*+——／＼〾〿]+" \
+                  "|[\"\'〝〞＂‟“”‘’＇｀‛〃〟„]" \
+                  "|[﹏—–〰〜＿]" \
+                  "|[\(\)\{\}\[\]\<\>＜＞｛｝［］《》〈〉（）「」『』【】〔〕〖〗〘〙〚〛｟｠｢｣]" \
+                  "|[⑴⑵⑶⑷⑸⑹⑺⑻]" \
+                  "|[a-zA-Z0-9]"
+    sentences = [clean_text(sentence, punctuation) for sentence in raw_textlines if
+                 (len(sentence) > 0) & (not sentence.isspace())]
+    word_matrix = [cut_words(sentence, stopwords) for sentence in sentences]
+    return word_matrix
 
-pca = PCA(n_components=2)
-new_vector = pca.fit_transform(vectors.values())
-new_vectors = []
-for index, key in enumerate(vectors.keys()):
-    # new_vectors[key] = new_vector[index]
-    new_vectors.append([key, new_vector[index][0], new_vector[index][1]])
 
-font = FontProperties(fname='/System/Library/Fonts/PingFang.ttc', size=8)
-for new_vector in new_vectors:
-    key, x, y = new_vector[0], new_vector[1], new_vector[2]
-    plt.text(x, y, key, fontproperties=font)
+def test1():
+    words = get_words()
+    w2v = W2V(words)
+    print '--------------词  频--------------'
+    bows = w2v.get_global_bow()
+    for bow in bows[:100]:
+        print bow[0], bow[1]
+    print '---------------------------------'
+    tfidf = w2v.get_tfidf()
+    # for tfidf in tfidf[:50]:
+    #     for item in tfidf:
+    #         print item[0], item[1]
+    print '---------------------------------'
+    gloabl_tfidf = w2v.get_global_tfidf()
+    vectors = {}
+    for tfidf in gloabl_tfidf[:100]:
+        vectors[tfidf[0]] = w2v.get_vector(tfidf[0])
+        print tfidf[0], tfidf[1]
 
-x_min = np.min(np.array(new_vectors)[:, 1].astype(float), axis=0)-0.5
-x_max = np.max(np.array(new_vectors)[:, 1].astype(float), axis=0)+0.5
-y_min = np.min(np.array(new_vectors)[:, 2].astype(float), axis=0)-0.5
-y_max = np.max(np.array(new_vectors)[:, 2].astype(float), axis=0)+0.5
-plt.ylim(y_min, y_max)
-plt.xlim(x_min, x_max)
-plt.show()
+    pca = PCA(n_components=2)
+    new_vector = pca.fit_transform(vectors.values())
+    new_vectors = []
+    for index, key in enumerate(vectors.keys()):
+        # new_vectors[key] = new_vector[index]
+        new_vectors.append([key, new_vector[index][0], new_vector[index][1]])
+
+    font = FontProperties(fname='/System/Library/Fonts/PingFang.ttc', size=8)
+    for new_vector in new_vectors:
+        key, x, y = new_vector[0], new_vector[1], new_vector[2]
+        plt.text(x, y, key, fontproperties=font)
+
+    x_min = np.min(np.array(new_vectors)[:, 1].astype(float), axis=0) - 0.5
+    x_max = np.max(np.array(new_vectors)[:, 1].astype(float), axis=0) + 0.5
+    y_min = np.min(np.array(new_vectors)[:, 2].astype(float), axis=0) - 0.5
+    y_max = np.max(np.array(new_vectors)[:, 2].astype(float), axis=0) + 0.5
+    plt.ylim(y_min, y_max)
+    plt.xlim(x_min, x_max)
+    plt.show()
+
+
+if __name__ == '__main__':
+    words = get_words()
+    w2v = W2V(words)
+    print w2v.get_corpus()
+    print w2v.get_global_bow(keyword=u'圣灵')
+    bows = w2v.get_global_bow(topn=10)
+    for token, bow in bows:
+        print token, bow
+    tfidfs = w2v.get_global_tfidf(topn=10)
+    for token, tfidf in tfidfs:
+        print token, tfidf
+    w2v.draw_vector(10)
